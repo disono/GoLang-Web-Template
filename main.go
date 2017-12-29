bfc0e5d40f863e43554e65fa0bb4c1651f964df2
Archie Disono <disonoarchie@gmail.com>
2017-12-29 19:31:36 +0800

    Initial Commit

--- main.go
+++ main.go
@@ -3,31 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	/* local variable definition */
-	var a int = 100
-	var b int = 200
 
-	fmt.Printf("Before swap, value of a : %d\n", a )
-	fmt.Printf("Before swap, value of b : %d\n", b )
-
-	/* calling a function to swap the values */
-	swap(a, b)
-
-	fmt.Printf("After swap, value of a : %d\n", a )
-	fmt.Printf("After swap, value of b : %d\n", b )
-}
-
-func multpleA(a string, b int) (string, int) {
-	return a,b
-}
-
-/* function definition to swap the values */
-func swap(x int, y int) int {
-	var temp int
-
-	temp = x /* save the value of x */
-	x = y    /* put y into x */
-	y = temp /* put temp into y */
-
-	return temp
 }
